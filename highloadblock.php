7ffe7f79246d3e2dc6ada0c7394794044554a510
Trixpk <89504082116@mail.ru>
2020-12-10 14:45:55 +0700

    Update highloadblock.php

--- highloadblock.php
+++ highloadblock.php
@@ -1,11 +1,14 @@
 <?
-
+  // Подключаем модуль
   \Bitrix\Main\Loader::includeModule('highloadblock');
-
+  // Получаем информацию о Highload блоке
   $hlData = \Bitrix\Highloadblock\HighloadBlockTable::getById(2)->fetch();
+  // Инициализируем класс сущности
   $hlEntity = \Bitrix\Highloadblock\HighloadBlockTable::compileEntity($hlData);
+  // Получаем имя таблицы
   $dataClass = $hlEntity->getDataClass();
 
+  // Работаем с методами таблицы
   $rsData = $dataClass::getList(array(
       'select' => array('ID', 'UF_NAME'),
       'order' => array('ID' => 'ASC'),
